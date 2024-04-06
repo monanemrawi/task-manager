3c7feaea04eb44602201a06cf19d4bcbefaecd11
mona <monanemrawi@gmail.com>
2024-04-06 12:24:44 +0300

    added delete

--- src/index.js
+++ src/index.js
@@ -117,7 +117,6 @@ app.get('/tasks/:id', async (req, res) => {
 })
 
 app.patch('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update) )
@@ -127,7 +126,7 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
         if (!task) {
             res.status(404).send()
@@ -140,7 +139,17 @@ app.patch('/tasks/:id', async (req, res) => {
 })
 
 app.delete('/tasks/:id', async (req, res) => {
-    
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 
 app.listen(port, () => {
