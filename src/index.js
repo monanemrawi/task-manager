f79d704ad2db0828e7750bd778f93a18337f2417
mona <monanemrawi@gmail.com>
2024-04-06 14:45:20 +0300

    set up jwts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/users')
-const Task = require('./models/tasks')
 const userRouter = require('./routers/users')
 const taskRouter = require('./routers/tasks')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+// app.use((req, res, next) => {
+//     if (req.method == 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site under maintenance')
+// })
 
 app.use(express.json())
 app.use(userRouter)
--- src/models/users.js
+++ src/models/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require ('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,6 +23,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true,
+        unique: true,
         lowercase: true,
         validate(value) {
             if (!validator.isEmail(value)) {
@@ -39,9 +41,43 @@ const userSchema = new mongoose.Schema({
                 throw new Eroor('Password can not contain "password"')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id : user._id.toString() }, 'passtogeneratetokens')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
 
