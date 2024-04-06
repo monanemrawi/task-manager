dda82806560602fcbe1b19ee5cd5a9f8968272d4
mona <monanemrawi@gmail.com>
2024-04-06 13:33:06 +0300

    fixed typo

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -51,7 +51,7 @@ router.patch('/tasks/:id', async (req, res) => {
     try {
         const task = await Task.findById(req.params.id)
         updates.forEach((update) => task[update] = req.body[update] )
-        await usser.save()
+        await task.save()
 
         if (!task) {
             res.status(404).send()
