ff88ed76af3fa434bedc31e01ea4824458c45b8a
mona <monanemrawi@gmail.com>
2024-04-12 11:04:05 +0300

    env variables

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const multer = require('multer')
 const sharp = require('sharp')
-const router = new express.Router()
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendCancelEmail } = require('../emails/account')
 const User = require('../models/users')
+const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch(e) {
@@ -74,6 +76,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.deleteOne({ _id: req.user._id });
+        sendCancelEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
